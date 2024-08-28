7f7f5541ea8fb30b9e15c2ac557ac2f3842fed86
gitviini <gvinicius105@gmail.com>
2024-08-27 23:16:20 -0300

    upload

--- manage/script.js
+++ manage/script.js
@@ -1,6 +1,25 @@
 const menu = document.querySelector('#menu')
-const container_menu = document.querySelector(".container-menu")
+const container_menu = document.querySelector(".container_menu")
+const sections = document.querySelectorAll('section')
+const cards = document.querySelector('.cards')
+const main = document.querySelector('main')
 
+cards.onload = () =>{
+    
+}
+
+sections_list = {}
+
+sections.forEach(section=>{
+    let a = document.createElement('a')
+    a.setAttribute('class','button')
+    a.setAttribute('href',`#${section.getAttribute('id')}`)
+    a.innerHTML = section.getAttribute('name')
+    a.onclick = () =>{
+        container_menu.classList.remove('click')
+    }
+    container_menu.appendChild(a)
+})
 
 menu.onclick = () =>{
     menu.classList.toggle('click')
